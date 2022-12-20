b313e34bfad428b0dbd9e183a61b77d95be6f8f0
Nicola Coretti <nicola.coretti@exasol.com>
2022-12-20 12:39:35 +0100

    Add templates

---
+++ exasol/toolbox/templates/.github/PULL_REQUEST_TEMPLATE/pull_request_template.md
@@ -0,0 +1,16 @@
+**< PR SPECIFIC CONTENT >**
+
+-------
+# ✔ Checklist(s)
+
+* [ ] Is the title of the Pull Request correct?
+* [ ] Is the title of the corresponding issue correct?
+* [ ] Have you updated the changelog?
+* [ ] Have you checked to ensure there aren't other open Pull Requests for the same update/change?
+* [ ] Are you mentioning the issue which this PullRequest fixes ("Fixes...")
+
+## 🔐 Security
+## 🐞 Bug
+## ✨ Feature
+## 🔧 Refactoring
+## 📚 Documentation
---
+++ exasol/toolbox/templates/noxconfig.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import Path
+from typing import (
+    Any,
+    MutableMapping,
+)
+
+from nox import Session
+
+
+@dataclass(frozen=True)
+class Config:
+    root: Path = Path(__file__).parent
+    doc: Path = Path(__file__).parent / "doc"
+    version_file: Path = Path(__file__).parent / "exasol" / "toolbox" / "version.py"
+
+    @staticmethod
+    def pre_integration_tests_hook(
+        _session: Session, _config: Config, _context: MutableMapping[str, Any]
+    ) -> bool:
+        """Implement if project specific behaviour is required"""
+        return True
+
+    @staticmethod
+    def post_integration_tests_hook(
+        _session: Session, _config: Config, _context: MutableMapping[str, Any]
+    ) -> bool:
+        """Implement if project specific behaviour is required"""
+        return True
+
+
+PROJECT_CONFIG = Config()
