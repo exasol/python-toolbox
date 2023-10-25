a16d785c579d342217ca24db54adac66de47dcf7
Nicola Coretti <nicola.coretti@exasol.com>
2023-10-25 10:23:29 +0200

    Change entry point for security command

--- exasol/toolbox/tools/tbx.py
+++ exasol/toolbox/tools/tbx.py
@@ -1,9 +1,14 @@
 import typer
 
-from exasol.toolbox.tools import workflow
+from exasol.toolbox.tools import (
+    security,
+    workflow,
+)
 
 CLI = typer.Typer()
 CLI.add_typer(workflow.CLI, name="workflow")
+CLI.add_typer(security.CLI, name="security")
+
 
 if __name__ == "__main__":
     CLI()
