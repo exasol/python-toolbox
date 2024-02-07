8c7d8d71808bbb8a841315d391e6f63b0541e340
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-02-07 08:02:21 +0100

    Prepare release 0.8.0 (#133)

--- test/unit/security_test.py
+++ test/unit/security_test.py
@@ -360,39 +360,46 @@ def test_convert_maven_input_no_vulnerable():  # pylint: disable=redefined-outer
     actual = set(security.from_maven("{}"))
     assert len(actual) == 0
 
+
 def test_format_jsonl():
     issue = security.Issue(
         coordinates="coordinates",
-        cve="cve", cwe="cwe",
-        description="description", 
-        references=()
+        cve="cve",
+        cwe="cwe",
+        description="description",
+        references=(),
     )
-    expected = json.dumps({ 
-        "cve": "cve",
-        "cwe": "cwe",
-        "description": "description",
-        "coordinates": "coordinates",
-        "references": [],
-        "url": "issue_url"
-    })
-    actual = security.format_jsonl("issue_url", issue)
+    expected = json.dumps(
+        {
+            "cve": "cve",
+            "cwe": "cwe",
+            "description": "description",
+            "coordinates": "coordinates",
+            "references": [],
+            "issue_url": "my_issue_url",
+        }
+    )
+    actual = security.format_jsonl("my_issue_url", issue)
     assert actual == expected
 
 
 def test_format_jsonl_removes_newline():
     issue = security.Issue(
         coordinates="coordinates",
-        cve="cve", cwe="cwe",
+        cve="cve",
+        cwe="cwe",
         description="description",
-        references=()
+        references=(),
     )
-    expected = json.dumps({
-        "cve": "cve",
-        "cwe": "cwe",
-        "description": "description",
-        "coordinates": "coordinates",
-        "references": [],
-        "url": "issue_url"
-    })
-    actual = security.format_jsonl("issue_url\n", issue)
+    expected = json.dumps(
+        {
+            "cve": "cve",
+            "cwe": "cwe",
+            "description": "description",
+            "coordinates": "coordinates",
+            "references": [],
+            "issue_url": "my_issue_url",
+        }
+    )
+    actual = security.format_jsonl("my_issue_url\n", issue)
     assert actual == expected
