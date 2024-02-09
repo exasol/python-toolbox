2b8373afcfc3c053e415d6af73d4af1d96c27601
Jannis Mittenzwei <jannis.mittenzwei@exasol.com>
2024-02-09 12:07:37 +0100

    Add command show, diff, update

--- exasol/toolbox/tools/issue.py
+++ exasol/toolbox/tools/issue.py
@@ -1,3 +1,5 @@
+import difflib
+import io
 from contextlib import ExitStack
 from pathlib import Path
 from typing import (
@@ -10,6 +12,7 @@ import importlib_resources as resources
 import typer
 from rich.columns import Columns
 from rich.console import Console
+from rich.syntax import Syntax
 
 stdout = Console()
 stderr = Console(stderr=True)
@@ -44,6 +47,47 @@ def list_issues(
 
     output = List(_issues()) if not columns else Columns(_issues(), expand=True)
     stdout.print(output)
+
+
+@CLI.command(name="show")
+def show_issue(
+    issue: str = typer.Argument(..., help="Issue which shall be shown."),
+) -> None:
+    """Shows a specific issue."""
+    issues = _issues()
+    if issue not in issues:
+        stdout.print(f"Unknown issue <{issue}>.", style="red")
+        raise typer.Exit(code=1)
+
+    issue = issues[issue]
+    stdout.print(Syntax.from_path(path=issue, encoding="utf-8", lexer="markdown"))  # type: ignore
+
+
+@CLI.command(name="diff")
+def diff_issue(
+    issue: str = typer.Argument(..., help="issue which shall be diffed."),
+    dest: Path = typer.Argument(
+        Path("./.github/ISSUE_TEMPLATE"),
+        help="target directory to diff the issue against.",
+    ),
+) -> None:
+    """Diff a specific issue against the installed one."""
+    issues = _issues()
+    if issue not in issues:
+        stdout.print(f"Unknown issue <{issue}>.", style="red")
+        raise typer.Exit(code=1)
+
+    # Use Any type to enable reuse of the variable/binding name
+    old: Any = dest / f"{issue}.md"
+    new: Any = Path(_issues()[issue])
+    with ExitStack() as stack:
+        old = stack.enter_context(open(old, encoding="utf-8") if old.exists() else io.StringIO(""))
+        new = stack.enter_context(open(new, encoding="utf-8"))
+        old = old.read().split("\n")
+        new = new.read().split("\n")
+
+    diff = difflib.unified_diff(old, new, fromfile="old", tofile="new")
+    stdout.print(Syntax("\n".join(diff), "diff"))
 
 
 def _install_issue(
@@ -97,6 +141,48 @@ def install_issue(
         destination = dest / f"{issue}.md"
         _install_issue(path, destination, exists_ok=True)
         stderr.print(f"Installed {issue} in {destination}")
+
+
+@CLI.command(name="update")
+def update_issue(
+    issue: str = typer.Argument("all", help="name of the issue to install."),
+    dest: Path = typer.Argument(
+        Path("./.github/ISSUE_TEMPLATE"), help="target directory to install the issue to."
+    ),
+    confirm: bool = typer.Option(
+        False, help="Automatically confirm overwritting exsisting issue(s)"
+    ),
+) -> None:
+    """Similar to install but checks for existing issues and shows diff"""
+    if not dest.exists():
+        dest.mkdir()
+
+    try:
+        issues = _select_issues(issue)
+    except Exception as ex:
+        stderr.print(f"[red]{ex}[/red]")
+        raise typer.Exit(-1)
+
+    if confirm:
+        install_issue(issue, dest)
+        raise typer.Exit(0)
+
+    for issue, path in issues.items():
+        destination = dest / f"{issue}.md"
+        try:
+            _install_issue(path, destination, exists_ok=False)
+            stderr.print(f"Updated {issue} in {destination}")
+        except Exception:
+            show_diff = typer.confirm(
+                f"issue <{issue}> already exists, show diff?"
+            )
+            if show_diff:
+                diff_issue(issue, dest)
+
+            overwrite = typer.confirm(f"Overwrite existing issue?")
+            if overwrite:
+                _install_issue(path, destination, exists_ok=True)
+                stderr.print(f"Updated {issue} in {destination}")
 
 
 if __name__ == "__main__":
