124bee20579e29dd093946aa835b391e82f74ab1
Nicola Coretti <nicola.coretti@exasol.com>
2024-02-09 10:46:45 +0100

    Add implementation for Version from string

--- exasol/toolbox/release/__init__.py
+++ exasol/toolbox/release/__init__.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 
+def _index_or(container, index, default):
+    try:
+        return container[index]
+    except IndexError:
+        return default
 
 @dataclass(frozen=True)
 class Version:
@@ -10,4 +15,6 @@ class Version:
 
     @staticmethod
     def from_string(version):
-        return Version(0,0,0)
+        parts = [int(number, base=0) for number in version.split(".")]
+        version = [_index_or(parts, i, 0) for i in range(3)]
+        return Version(*version)
