c84e26e92062244b3dd6c1103922f9cea13454ec
Nicola Coretti <nicola.coretti@exasol.com>
2024-02-09 11:04:44 +0100

    Add implementation for version comparision

--- exasol/toolbox/release/__init__.py
+++ exasol/toolbox/release/__init__.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import total_ordering
 
 
 def _index_or(container, index, default):
@@ -8,11 +9,30 @@ def _index_or(container, index, default):
         return default
 
 
+@total_ordering
 @dataclass(frozen=True)
 class Version:
     major: int
     minor: int
     patch: int
+
+    def __lt__(self, other):
+        return (
+            self.major < other.major
+            or (self.major <= other.major and self.minor < other.minor)
+            or (
+                self.major <= other.major
+                and self.minor <= other.minor
+                and self.patch < other.patch
+            )
+        )
+
+    def __eq__(self, other):
+        return (
+            self.major == other.major
+            and self.minor == other.minor
+            and self.patch == other.patch
+        )
 
     @staticmethod
     def from_string(version):
