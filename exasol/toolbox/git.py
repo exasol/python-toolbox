90c7351f85ca6d2966321aa31bef8bcd3e2dcb23
Nicola Coretti <nicola.coretti@exasol.com>
2024-03-13 08:31:04 +0100

    Update exasol/toolbox/git.py
    Co-authored-by: Christoph Kuhnke <github@kuhnke.net>

--- exasol/toolbox/git.py
+++ exasol/toolbox/git.py
@@ -6,8 +6,7 @@ def tags() -> Iterable[str]:
     """
     Returns a list of all tags, sorted from [0] oldest to [-1] newest.
     PreConditions:
-    - the git cli tool needs to be installed
-    - the git cli tool can be found via $PATH
+    - the git cli tool is installed and can be found via `$PATH`
     - the code is executed where the working directory is within a git repository
     """
     command = ["git", "tag", "--sort=committerdate"]
