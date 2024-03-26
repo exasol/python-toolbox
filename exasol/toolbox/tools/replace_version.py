aee318c9b130834871dd4f044efbc40f154498c1
Jannis Mittenzwei <jannis.mittenzwei@exasol.com>
2024-03-26 11:59:45 +0100

    replace_version.py

---
+++ exasol/toolbox/tools/replace_version.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+
+def replace_version(template: Path, version: str) -> None:
+    with open(template, "r+", encoding="utf8") as file:
+        lines = file.readlines()
+        _replace(lines, "exasol/python-toolbox/.github/", version)
+        file.seek(0)
+        file.writelines(lines)
+
+
+def _replace(lines: list[str], replace_filter: str, version: str) -> None:
+    for count, line in enumerate(lines):
+        if line.find(replace_filter) != -1:
+            if line.find("@") != -1:
+                lines[count] = line[0:line.index("@")+1] + version + "\n"
