51d1f0b3cb55c42ee2bcec20f2a55277427c10d2
Jannis Mittenzwei <jannis.mittenzwei@exasol.com>
2024-03-26 18:03:11 +0100

    fix type error

--- exasol/toolbox/tools/replace_version.py
+++ exasol/toolbox/tools/replace_version.py
@@ -1,32 +1,53 @@
 from pathlib import Path
 from typing import List
 
+
 def replace_version(template: Path, version: str) -> None:
-    with open(template, "r", encoding="utf-8") as file:
+    with open(template, encoding="utf-8") as file:
         input_lines = file.readlines()
 
-    output_lines = _replace_version(input_lines, "exasol/python-toolbox/.github/", version)
+    output_lines = _replace_version(
+        input_lines, "exasol/python-toolbox/.github/", version
+    )
 
-    with open(template, "w", encoding="utf-8")as file:
+    with open(template, "w", encoding="utf-8") as file:
         file.writelines(output_lines)
+
 
 def _replace_version(input_lines, replace_filter, version) -> list[str]:
     filtered_lines = _filter_replace_lines(input_lines, replace_filter)
     replaced_filtered_lines = _replace_filtered_line(filtered_lines, version)
     return _replace_lines(input_lines, replaced_filtered_lines)
 
-def _filter_replace_lines(input_lines, replace_filter) -> list[tuple[int, str]]:
-    filtered_lines = ((index, line) for index, line in enumerate(input_lines) if line.find(replace_filter) != -1)
-    filtered_lines = [(index, line) for index, line in filtered_lines if line.find("@") != -1]
-    return filtered_lines
 
-def _replace_filtered_line(filtered_lines: list[tuple[int, str]], version: str) -> list[tuple[int, str]]:
-    return [(index, line[0:line.index("@")+1]+version+"\n") for index, line in filtered_lines]
+def _filter_replace_lines(
+    input_lines: str, replace_filter: str
+) -> list[tuple[int, str]]:
+    filtered_lines = (
+        (index, line)
+        for index, line in enumerate(input_lines)
+        if line.find(replace_filter) != -1
+    )
+    filtered_filtered_lines = [
+        (index, line) for index, line in filtered_lines if line.find("@") != -1
+    ]
+    return filtered_filtered_lines
+
+
+def _replace_filtered_line(
+    filtered_lines: list[tuple[int, str]], version: str
+) -> list[tuple[int, str]]:
+    return [
+        (index, line[0 : line.index("@") + 1] + version + "\n")
+        for index, line in filtered_lines
+    ]
+
 
 def _replace_lines(lines: list[str], replace_lines: list[tuple[int, str]]):
     output = lines
     for index, line in replace_lines:
         output[index] = line
     return output
+
 
 replace_version(Path("foo/ci.yml"), "9.9.9")
