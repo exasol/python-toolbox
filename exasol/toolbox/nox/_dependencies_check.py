1ffe47d9783d66b864a02c305321b4ebd4e53c06
Jannis Mittenzwei <mittenzwei.j4@gmail.com>
2024-09-03 10:09:44 +0200

    add nox task + test

---
+++ exasol/toolbox/nox/_dependencies_check.py
@@ -0,0 +1,69 @@
+import tomlkit
+import sys
+from pathlib import Path
+import nox
+from nox import Session
+
+
+FILTERS = ['url', 'git', 'path']
+
+
+@nox.session(name="dependencies-check", python=False)
+def dependency_check(session: Session):
+    file = Path("pyproject.toml")
+    sys.exit(_dependencies_check(file.read_text()))
+
+
+def _source_filter(version, filters):
+    output = None
+    if isinstance(version, dict):
+        for key in version.keys():
+            if key in filters:
+                output = key
+    return output
+
+
+def _dependencies_check(string: str):
+    toml = tomlkit.loads(string)
+    dependencies: list = []
+    dev_dependencies: list = []
+    group_dependencies: dict = {}
+    if "tool" in toml:
+        if "poetry" in toml["tool"].unwrap():
+            if "dependencies" in toml["tool"].unwrap()["poetry"]:
+                for name, version in toml["tool"].unwrap()["poetry"]["dependencies"].items():
+                    key = _source_filter(version, FILTERS)
+                    if key:
+                        dependencies.append(f"{name} = {version}")
+
+            if "dev" in toml["tool"].unwrap()["poetry"]:
+                if "dependencies" in toml["tool"].unwrap()["poetry"]["dev"]:
+                    for name, version in toml["tool"].unwrap()["poetry"]["dev"]["dependencies"].items():
+                        key = _source_filter(version, FILTERS)
+                        if key:
+                            dev_dependencies.append(f"{name} = {version}")
+
+            if "group" in toml["tool"].unwrap()["poetry"]:
+                for group in toml["tool"].unwrap()["poetry"]["group"]:
+                    if "dependencies" in toml["tool"].unwrap()["poetry"]["group"][group]:
+                        for name, version in toml["tool"].unwrap()["poetry"]["group"][group]["dependencies"].items():
+                            key = _source_filter(version, FILTERS)
+                            if key:
+                                if f'[tool.poetry.group.{group}.dependencies]' not in group_dependencies:
+                                    group_dependencies[f'[tool.poetry.group.{group}.dependencies]'] = []
+                                group_dependencies[f'[tool.poetry.group.{group}.dependencies]'].append(f"{name} = {version}")
+
+    if dependencies or dev_dependencies or group_dependencies:
+        l = len(dependencies)
+        m = len(dev_dependencies)
+        n = 0
+        for _, dependency in group_dependencies.items():
+            n += len(dependency)
+        suffix = "y" if l+m+n == 1 else "ies"
+        output = f"{l+m+n} illegal dependenc{suffix}:\n"
+        output += ("\n[tool.poetry.dependencies]\n"+"\n".join(dependencies)+"\n") if l > 0 else ""
+        output += ("\n[tool.poetry.dev.dependencies]\n"+"\n".join(dev_dependencies)+"\n") if m > 0 else ""
+        output += ("\n".join(f"\n{key}\n{'\n'.join(value)}"for key, value in group_dependencies.items())) if n > 0 else ""
+        output += "\n"
+        return output
+    return 0
--- test/unit/dependencies_check_test.py
+++ test/unit/dependencies_check_test.py
@@ -1,37 +1,79 @@
 from tomlkit import loads
 import pytest
-from exasol.toolbox.tools.dependencies_check import _source_filter
+from exasol.toolbox.nox._dependencies_check import (
+    _source_filter,
+    _dependencies_check
+)
+import pathlib
+
 
 @pytest.mark.parametrize(
-    "filter,source,expected",
+    "filters,source,expected",
     [
         (
-            ['url', 'git', 'path'],
-            """example-url = {url = "https://example.com/my-package-0.1.0.tar.gz"}""",
-            'url'),
+                ['url', 'git', 'path'],
+                """example-url = {url = "https://example.com/my-package-0.1.0.tar.gz"}""",
+                'url'),
         (
-            ['url', 'git', 'path'],
-            """example-git = {git = "git@github.com:requests/requests.git"}""",
-            'git'),
+                ['url', 'git', 'path'],
+                """example-git = {git = "git@github.com:requests/requests.git"}""",
+                'git'),
         (
-            ['url', 'git', 'path'],
-            """example-path = {path = "../my-package/dist/my-package-0.1.0.tar.gz"}""",
-            'path'),
+                ['url', 'git', 'path'],
+                """example-path = {path = "../my-package/dist/my-package-0.1.0.tar.gz"}""",
+                'path'),
         (
-            ['url', 'git', 'path'],
-            """example-url = {platform = "darwin", url = "https://example.com/my-package-0.1.0.tar.gz"}""",
-            'url'),
+                ['url', 'git', 'path'],
+                """example-url = {platform = "darwin", url = "https://example.com/my-package-0.1.0.tar.gz"}""",
+                'url'),
         (
-            ['url', 'git', 'path'],
-            """example = "^2.31.0.6" """,
-            None),
+                ['url', 'git', 'path'],
+                """example = "^2.31.0.6" """,
+                None),
         (
-            ['url', 'git', 'path'],
-            """python = ">=3.8.0,<4.0" """,
-            None
+                ['url', 'git', 'path'],
+                """python = ">=3.8.0,<4.0" """,
+                None
         )
     ]
 )
-def test_dependencies_check(filter, source, expected):
+def test_dependencies_check(filters, source, expected):
     for _, version in loads(source).items():
         assert _source_filter(version, filter) == expected
+
+
+@pytest.mark.parametrize(
+    "toml, expected",
+    [
+        (
+                """[tool.poetry.dependencies]
+python = "^3.8"
+example-url = {url = "https://example.com/my-package-0.1.0.tar.gz"}
+
+[tool.poetry.dev.dependencies]
+nox = ">=2022.8.7"
+
+[tool.poetry.group.test.dependencies]
+sphinx = ">=5.3,<8"
+example-git = {git = "git@github.com:requests/requests.git"}
+
+[tool.poetry.group.dev.dependencies]
+pytest = ">=7.2.2,<9"
+example-path = {path = "../my-package/dist/my-package-0.1.0.tar.gz"}
+""",
+                """3 illegal dependencies:
+
+[tool.poetry.dependencies]
+example-url = {'url': 'https://example.com/my-package-0.1.0.tar.gz'}
+
+[tool.poetry.group.test.dependencies]
+example-git = {'git': 'git@github.com:requests/requests.git'}
+
+[tool.poetry.group.dev.dependencies]
+example-path = {'path': '../my-package/dist/my-package-0.1.0.tar.gz'}
+"""
+        ),
+    ]
+)
+def test_dependencies_check(toml, expected):
+    assert _dependencies_check(toml) == expected
