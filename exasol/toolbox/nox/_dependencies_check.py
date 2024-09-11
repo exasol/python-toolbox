5f41253677b30d5d69406104ae7c1a1e83bb2fb2
Jannis Mittenzwei <mittenzwei.j4@gmail.com>
2024-09-11 14:55:44 +0200

    resolves the conversations

--- exasol/toolbox/nox/_dependencies_check.py
+++ exasol/toolbox/nox/_dependencies_check.py
@@ -4,68 +4,81 @@ from pathlib import Path
 import nox
 from nox import Session
 from noxconfig import PROJECT_CONFIG
-from typing import List
-
-
-FILTERS = ['url', 'git', 'path']
+from typing import (
+    List,
+    Dict
+)
+import rich.console
 
 
 @nox.session(name="dependencies-check", python=False)
 def dependency_check(session: Session) -> None:
-    file = Path(PROJECT_CONFIG.root, "pyproject.toml")
-    output = _dependencies_check(file.read_text())
-    print("\033[31m"+output) if output else print("\033[32m"+"Success: no wrong dependencies found")
-    sys.exit(0 if not output else 1)
+    content = Path(PROJECT_CONFIG.root, "pyproject.toml").read_text()
+    dependencies = DependenciesCheck(content).parse()
+    console = rich.console.Console()
+    if dependencies.illegal():
+        dependencies.report_illegal(console)
+        sys.exit(1)
+    dependencies.report_illegal(console)
+    sys.exit(0)
 
 
-def _source_filter(version, filters) -> bool:
-    for f in filters:
-        if f in version:
-            return True
-    return False
+class DependenciesCheck:
+    ILLEGAL_DEPENDENCIES = ['url', 'git', 'path']
 
+    def __init__(self, pyproject_toml: str):
+        self.illegal_dict: Dict[str, List[str]] | None = None
+        self.content = pyproject_toml
 
-def extract_dependencies(section, filters) -> List[str]:
-    dependencies = []
-    for name, version in section.items():
-        if _source_filter(version, filters):
-            dependencies.append(f"{name} = {version}")
-    return dependencies
+    def parse(self) -> "DependenciesCheck":
+        def source_filter(version, filters) -> bool:
+            for f in filters:
+                if f in version:
+                    return True
+            return False
 
+        def extract_dependencies(section, filters) -> List[str]:
+            dependencies = []
+            for name, version in section.items():
+                if source_filter(version, filters):
+                    dependencies.append(f"{name} = {version}")
+            return dependencies
 
-def _dependencies_check(string: str) -> str:
-    toml = tomlkit.loads(string)
-    group_dependencies = {}
+        illegal: Dict[str, List[str]] = {}
+        toml = tomlkit.loads(self.content)
+        poetry = toml.get("tool", {}).get("poetry", {})
 
-    poetry = toml.get("tool", {}).get("poetry", {})
+        part = poetry.get("dependencies", {})
+        dependencies_list = extract_dependencies(part, self.ILLEGAL_DEPENDENCIES)
+        if dependencies_list:
+            illegal["tool.poetry.dependencies"] = dependencies_list
 
-    dependencies = extract_dependencies(poetry.get("dependencies", {}), FILTERS)
+        part = poetry.get("dev", {}).get("dependencies", {})
+        dependencies_list = extract_dependencies(part, self.ILLEGAL_DEPENDENCIES)
+        if dependencies_list:
+            illegal["tool.poetry.dev.dependencies"] = dependencies_list
 
-    dev_section = poetry.get("dev", {}).get("dependencies", {})
-    dev_dependencies = extract_dependencies(dev_section, FILTERS)
+        part = poetry.get("group", {})
+        for group, content in part.items():
+            dependencies_list = extract_dependencies(content.get("dependencies", {}), self.ILLEGAL_DEPENDENCIES)
+            if dependencies_list:
+                illegal[f"tool.poetry.group.{group}.dependencies"] = dependencies_list
 
-    group_section = poetry.get("group", {})
-    for group, content in group_section.items():
-        group_deps = extract_dependencies(content.get("dependencies", {}), FILTERS)
-        if group_deps:
-            group_key = f'[tool.poetry.group.{group}.dependencies]'
-            group_dependencies[group_key] = group_deps
+        self.illegal_dict = illegal
+        return self
 
-    total_count = len(dependencies) + len(dev_dependencies) + sum(len(deps) for deps in group_dependencies.values())
-    if total_count > 0:
-        suffix = "y" if total_count == 1 else "ies"
-        output = [f"{total_count} illegal dependenc{suffix}:\n"]
+    def report_illegal(self, console: rich.console.Console):
+        if self.illegal_dict:
+            count = sum(len(deps) for deps in self.illegal_dict.values())
+            suffix = "y" if count == 1 else "ies"
+            console.print(f"{count} illegal dependenc{suffix}\n", style="red")
+            for section, dependencies in self.illegal_dict.items():
+                console.print(f"\\[{section}]", style="red")
+                for dependency in dependencies:
+                    console.print(dependency, style="red")
+                console.print("")
+        else:
+            console.print("Success: All dependencies refer to explicit pipy releases.", style="green")
 
-        if dependencies:
-            output.append(f"\n[tool.poetry.dependencies]\n" + "\n".join(dependencies) + "\n")
-        if dev_dependencies:
-            output.append(f"\n[tool.poetry.dev.dependencies]\n" + "\n".join(dev_dependencies) + "\n")
-        for key, value in group_dependencies.items():
-            output.append(f"\n{key}\n" + "\n".join(value) + "\n")
-
-        return "".join(output)
-    return ""
-
-
-if __name__ == "__main__":
-    print(_dependencies_check(Path("pyproject.toml").read_text()))
+    def illegal(self) -> Dict[str, List[str]] | None:
+        return self.illegal_dict
--- test/unit/dependencies_check_test.py
+++ test/unit/dependencies_check_test.py
@@ -1,56 +1,114 @@
-from tomlkit import loads
 import pytest
-from exasol.toolbox.nox._dependencies_check import (
-    _source_filter,
-    _dependencies_check
-)
+from exasol.toolbox.nox._dependencies_check import DependenciesCheck
 
 
 @pytest.mark.parametrize(
-    "filters,source,expected",
+    "toml,expected",
     [
         (
-                ['url', 'git', 'path'],
-                """example-url = {url = "https://example.com/my-package-0.1.0.tar.gz"}""",
-                'url'),
+            """
+            """,
+            {}
+        ),
         (
-                ['url', 'git', 'path'],
-                """example-git = {git = "git@github.com:requests/requests.git"}""",
-                'git'),
-        (
-                ['url', 'git', 'path'],
-                """example-path = {path = "../my-package/dist/my-package-0.1.0.tar.gz"}""",
-                'path'),
-        (
-                ['url', 'git', 'path'],
-                """example-url = {platform = "darwin", url = "https://example.com/my-package-0.1.0.tar.gz"}""",
-                'url'),
-        (
-                ['url', 'git', 'path'],
-                """example = "^2.31.0.6" """,
-                None),
-        (
-                ['url', 'git', 'path'],
-                """python = ">=3.8.0,<4.0" """,
-                None
-        )
-    ]
-)
-def test_dependencies_check(filters, source, expected):
-    for _, version in loads(source).items():
-        assert _source_filter(version, filter) == expected
-
-
-@pytest.mark.parametrize(
-    "toml, expected",
-    [
-        (
-                """[tool.poetry.dependencies]
+            """
+[tool.poetry.dependencies]
 python = "^3.8"
-example-url = {url = "https://example.com/my-package-0.1.0.tar.gz"}
+example-url1 = {url = "https://example.com/my-package-0.1.0.tar.gz"}
 
 [tool.poetry.dev.dependencies]
 nox = ">=2022.8.7"
+example-url2 = {url = "https://example.com/my-package-0.2.0.tar.gz"}
+
+[tool.poetry.group.test.dependencies]
+sphinx = ">=5.3,<8"
+example-git = {git = "git@github.com:requests/requests.git"}
+
+[tool.poetry.group.dev.dependencies]
+pytest = ">=7.2.2,<9"
+example-path1 = {path = "../my-package/dist/my-package-0.1.0.tar.gz"}
+            """,
+            {
+                "tool.poetry.dependencies": ["example-url1 = {'url': 'https://example.com/my-package-0.1.0.tar.gz'}"],
+                "tool.poetry.dev.dependencies": ["example-url2 = {'url': 'https://example.com/my-package-0.2.0.tar.gz'}"],
+                "tool.poetry.group.test.dependencies": ["example-git = {'git': 'git@github.com:requests/requests.git'}"],
+                "tool.poetry.group.dev.dependencies": ["example-path1 = {'path': '../my-package/dist/my-package-0.1.0.tar.gz'}"],
+            }
+        ),
+        (
+            """
+[tool.poetry.dev.dependencies]
+nox = ">=2022.8.7"
+example-url2 = {url = "https://example.com/my-package-0.2.0.tar.gz"}
+
+[tool.poetry.group.test.dependencies]
+sphinx = ">=5.3,<8"
+example-git = {git = "git@github.com:requests/requests.git"}
+
+[tool.poetry.group.dev.dependencies]
+pytest = ">=7.2.2,<9"
+example-path1 = {path = "../my-package/dist/my-package-0.1.0.tar.gz"}
+            """,
+            {
+                "tool.poetry.dev.dependencies": ["example-url2 = {'url': 'https://example.com/my-package-0.2.0.tar.gz'}"],
+                "tool.poetry.group.test.dependencies": ["example-git = {'git': 'git@github.com:requests/requests.git'}"],
+                "tool.poetry.group.dev.dependencies": ["example-path1 = {'path': '../my-package/dist/my-package-0.1.0.tar.gz'}"],
+            }
+        ),
+        (
+            """
+[tool.poetry.dependencies]
+python = "^3.8"
+example-url1 = {url = "https://example.com/my-package-0.1.0.tar.gz"}
+
+[tool.poetry.group.test.dependencies]
+sphinx = ">=5.3,<8"
+example-git = {git = "git@github.com:requests/requests.git"}
+
+[tool.poetry.group.dev.dependencies]
+pytest = ">=7.2.2,<9"
+example-path1 = {path = "../my-package/dist/my-package-0.1.0.tar.gz"}
+            """,
+            {
+                "tool.poetry.dependencies": ["example-url1 = {'url': 'https://example.com/my-package-0.1.0.tar.gz'}"],
+                "tool.poetry.group.test.dependencies": ["example-git = {'git': 'git@github.com:requests/requests.git'}"],
+                "tool.poetry.group.dev.dependencies": ["example-path1 = {'path': '../my-package/dist/my-package-0.1.0.tar.gz'}"],
+            }
+        ),
+        (
+            """
+[tool.poetry.dependencies]
+python = "^3.8"
+example-url1 = {url = "https://example.com/my-package-0.1.0.tar.gz"}
+
+[tool.poetry.dev.dependencies]
+nox = ">=2022.8.7"
+example-url2 = {url = "https://example.com/my-package-0.2.0.tar.gz"}
+            """,
+            {
+                "tool.poetry.dependencies": ["example-url1 = {'url': 'https://example.com/my-package-0.1.0.tar.gz'}"],
+                "tool.poetry.dev.dependencies": ["example-url2 = {'url': 'https://example.com/my-package-0.2.0.tar.gz'}"],
+            }
+        )
+    ]
+)
+def test_dependency_check_parse(toml, expected):
+    dependencies = DependenciesCheck(toml).parse()
+    assert dependencies.illegal() == expected
+
+
+@pytest.mark.parametrize(
+    "toml,expected",
+    [
+        (
+            """
+[tool.poetry.dependencies]
+python = "^3.8"
+example-url1 = {url = "https://example.com/my-package-0.1.0.tar.gz"}
+
+[tool.poetry.dev.dependencies]
+nox = ">=2022.8.7"
+example-url2 = {url = "https://example.com/my-package-0.2.0.tar.gz"}
 
 [tool.poetry.group.test.dependencies]
 sphinx = ">=5.3,<8"
@@ -60,21 +118,52 @@ example-git = {git = "git@github.com:requests/requests.git"}
 pytest = ">=7.2.2,<9"
 example-path1 = {path = "../my-package/dist/my-package-0.1.0.tar.gz"}
 example-path2 = {path = "../my-package/dist/my-package-0.2.0.tar.gz"}
-""",
-                """4 illegal dependencies:
+            """,
+            """5 illegal dependencies
 
-[tool.poetry.dependencies]
-example-url = {'url': 'https://example.com/my-package-0.1.0.tar.gz'}
+\\[tool.poetry.dependencies]
+example-url1 = {'url': 'https://example.com/my-package-0.1.0.tar.gz'}
 
-[tool.poetry.group.test.dependencies]
+\\[tool.poetry.dev.dependencies]
+example-url2 = {'url': 'https://example.com/my-package-0.2.0.tar.gz'}
+
+\\[tool.poetry.group.test.dependencies]
 example-git = {'git': 'git@github.com:requests/requests.git'}
 
-[tool.poetry.group.dev.dependencies]
+\\[tool.poetry.group.dev.dependencies]
 example-path1 = {'path': '../my-package/dist/my-package-0.1.0.tar.gz'}
 example-path2 = {'path': '../my-package/dist/my-package-0.2.0.tar.gz'}
+
+"""
+        ),
+        (
+            """
+[tool.poetry.dependencies]
+python = "^3.8"
+
+[tool.poetry.dev.dependencies]
+nox = ">=2022.8.7"
+
+[tool.poetry.group.test.dependencies]
+sphinx = ">=5.3,<8"
+
+[tool.poetry.group.dev.dependencies]
+pytest = ">=7.2.2,<9"
+            """,
+            """Success: All dependencies refer to explicit pipy releases.
 """
         ),
     ]
 )
-def test_dependencies_check(toml, expected):
-    assert _dependencies_check(toml) == expected
+def test_dependencies_check_report(toml, expected):
+    class Console:
+        def __init__(self):
+            self.output = ""
+
+        def print(self, output: str, style: str = None):
+            self.output += output + "\n"
+
+    console = Console()
+    dependencies = DependenciesCheck(toml).parse()
+    dependencies.report_illegal(console)
+    assert console.output == expected
