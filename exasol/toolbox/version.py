9ff133ec9d82ed96769c803114428140f878e7c7
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2025-10-10 08:52:02 -0300

    Prepare release 1.10.0

--- exasol/toolbox/version.py
+++ exasol/toolbox/version.py
@@ -9,7 +9,7 @@ If you need to change the version, do so in the pyproject.toml, e.g. by using
 """
 
 MAJOR = 1
-MINOR = 9
+MINOR = 10
 PATCH = 0
 VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
 __version__ = VERSION
