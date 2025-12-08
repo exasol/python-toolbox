4b4f056a13e53ec509d596d5c13b8112461b129d
Ariel Schulz <ariel.schulz@exasol.com>
2025-12-08 14:30:59 +0100

    Simplify test

--- test/unit/nox/_documentation_test.py
+++ test/unit/nox/_documentation_test.py
@@ -12,81 +12,78 @@ from exasol.toolbox.nox._documentation import (
 from noxconfig import PROJECT_CONFIG
 
 
-@pytest.fixture()
-def file1():
-    return """
-https://examle.invalid
-:ref:`Test`"""
+@pytest.fixture
+def index(config):
+    index_rst = config.documentation_path / "index.rst"
+    text = """
+    .. _Test:
+
+    Test
+    ____test_docs_links
+
+    .. toctree::
+       :maxdepth: 1
+       :hidden:
+
+       file
+   """
+    index_rst.write_text(text)
 
 
-@pytest.fixture()
-def index():
-    return """.. _Test:
+@pytest.fixture
+def config(test_project_config_factory):
+    config = test_project_config_factory()
 
-Test
-____
+    # set up required file for Sphinx
+    doc_path = config.documentation_path
+    doc_path.mkdir(parents=True, exist_ok=True)
+    shutil.copyfile(PROJECT_CONFIG.documentation_path / "conf.py", doc_path / "conf.py")
 
-.. toctree::
-   :maxdepth: 1
-   :hidden:
-
-   file"""
+    return config
 
 
-@pytest.fixture()
-def expected1():
-    return """filename: file.rst:2 -> uri: https://examle.invalid"""
+@pytest.fixture
+def set_up_doc_with_link(config, index):
+    dummy_rst = config.documentation_path / "dummy.rst"
+    dummy_rst.write_text("https://examle.invalid\n:ref:`Test`")
 
 
-def config(index, file, tmp_path):
-    test_doc = tmp_path / "doc"
-    test_doc.mkdir()
-    (test_doc / "_static").mkdir()
-    shutil.copyfile(PROJECT_CONFIG.documentation_path / "conf.py", test_doc / "conf.py")
-    rst_index = test_doc / "index.rst"
-    rst_file1 = test_doc / "file.rst"
-    rst_index.touch()
-    rst_file1.touch()
-    rst_index.write_text(index)
-    rst_file1.write_text(file)
+def test_docs_links(config, set_up_doc_with_link):
+    r_code, text = _docs_list_links(config.documentation_path)
 
-
-def test_docs_links(index, file1, expected1, tmp_path):
-    config(index, file1, tmp_path)
-    r_code, text = _docs_list_links(tmp_path / "doc")
-    assert (text == expected1) and not r_code
+    assert not r_code
+    assert text == """filename: dummy.rst:1 -> uri: https://examle.invalid"""
 
 
 @pytest.mark.parametrize(
-    "file2, expected2",
+    "file_content, expected_code, expected_message",
     [
-        ("https://github.com/exasol/python-toolbox", (0, "")),
+        ("https://github.com/exasol/python-toolbox", 0, ""),
         (
             "http://nox.thea.codes/en/stable/",
-            (
-                0,
-                "file.rst:1: [redirected with Found] http://nox.thea.codes/en/stable/ to https://nox.thea.codes/en/stable/\n",
-            ),
+            0,
+            "[redirected with Found] http://nox.thea.codes/en/stable/ to https://nox.thea.codes/en/stable/\n",
         ),
         (
             "https://github.com/exasol/python-toolbox/pull",
-            (
-                0,
-                "file.rst:1: [redirected permanently] https://github.com/exasol/python-toolbox/pull to https://github.com/exasol/python-toolbox/pulls\n",
-            ),
+            0,
+            "[redirected permanently] https://github.com/exasol/python-toolbox/pull to https://github.com/exasol/python-toolbox/pulls\n",
         ),
         (
             "https://github.com/exasol/python-toolbox/asdf",
-            (
-                1,
-                "file.rst:1: [broken] https://github.com/exasol/python-toolbox/asdf: 404 Client Error: Not Found for url: https://github.com/exasol/python-toolbox/asdf\n",
-            ),
+            1,
+            "[broken] https://github.com/exasol/python-toolbox/asdf: 404 Client Error: Not Found for url: https://github.com/exasol/python-toolbox/asdf\n",
         ),
     ],
 )
-def test_docs_links_check(index, file2, expected2, tmp_path):
-    config(index, file2, tmp_path)
+def test_docs_links_check(config, index, file_content, expected_code, expected_message):
+    dummy_rst = config.documentation_path / "dummy.rst"
+    dummy_rst.write_text(file_content)
+
     args = MagicMock
     args.output = None
-    actual = _docs_links_check(tmp_path / "doc", args)
-    assert actual == expected2
+
+    code, message = _docs_links_check(config.documentation_path, args)
+
+    assert code == expected_code
+    assert expected_message in message
