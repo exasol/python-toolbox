edd7d560b188eda2d0bf55201fce7be364249a32
Ariel Schulz <ariel.schulz@exasol.com>
2026-01-27 15:44:08 +0100

    Add custom css

--- doc/conf.py
+++ doc/conf.py
@@ -73,6 +73,8 @@ html_theme = "shibuya"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
+
 html_title = "Toolbox"
 html_theme_options = {
     "light_logo": "_static/light-exasol-logo.svg",
