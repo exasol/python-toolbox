c3aef077add6abb861ad959ce17192dbf8f3c45e
Ariel Schulz <ariel.schulz@exasol.com>
2026-02-05 10:33:26 +0100

    Update test to split out concerns so clearer for future usage

--- test/unit/util/workflows/template_test.py
+++ test/unit/util/workflows/template_test.py
@@ -3,98 +3,176 @@ from inspect import cleandoc
 from exasol.toolbox.util.workflows.template import TemplateToWorkflow
 from noxconfig import PROJECT_CONFIG
 
-TEMPLATE = """
-name: Build & Publish
 
-on:
-  workflow_call:
-    secrets:
-      PYPI_TOKEN:
-          required: true
+class TestTemplateToWorkflow:
+    @staticmethod
+    def test_works_for_general_case():
+        input_yaml = """
+        name: Build & Publish
 
-jobs:
-  cd-job:
-    name: Continuous Delivery
-    runs-on: "(( os_version ))"
-    permissions:
-      contents: write
-    steps:
-      # Comment in nested area
-      - name: SCM Checkout # Comment inline
-        uses: actions/checkout@v6
-        # Comment in step
-      - name: Setup Python & Poetry Environment
-        uses: exasol/python-toolbox/.github/actions/python-environment@v5
-        with:
-          python-version: "(( minimum_python_version ))"
-          poetry-version: "(( dependency_manager_version ))"
-      - name: Build Artifacts
-        run: poetry build
-      - name: PyPi Release
-        env:
-          POETRY_HTTP_BASIC_PYPI_USERNAME: "__token__"
-          POETRY_HTTP_BASIC_PYPI_PASSWORD: "${{ secrets.PYPI_TOKEN }}"
-        run: poetry publish
-      - name: GitHub Release
-        env:
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-        run: >
-          gh release create ${GITHUB_REF_NAME}
-          --title ${GITHUB_REF_NAME}
-          --notes-file ./doc/changes/changes_${GITHUB_REF_NAME}.md
-          dist/*
+        on:
+          workflow_call:
+            secrets:
+              PYPI_TOKEN:
+                required: true
 
-"""
+        jobs:
+          cd-job:
+            name: Continuous Delivery
+            permissions:
+              contents: write
+        """
 
-WORKFLOW = """
-name: Build & Publish
+        template_to_workflow = TemplateToWorkflow(
+            template_str=cleandoc(input_yaml),
+            github_template_dict=PROJECT_CONFIG.github_template_dict,
+        )
+        assert template_to_workflow.convert() == cleandoc(input_yaml)
 
-on:
-  workflow_call:
-    secrets:
-      PYPI_TOKEN:
-        required: true
+    @staticmethod
+    def test_fixes_extra_horizontal_whitespace():
+        # required has 2 extra spaces
+        input_yaml = """
+        name: Build & Publish
 
-jobs:
-  cd-job:
-    name: Continuous Delivery
-    runs-on: "ubuntu-24.04"
-    permissions:
-      contents: write
-    steps:
-      # Comment in nested area
-      - name: SCM Checkout # Comment inline
-        uses: actions/checkout@v6
-        # Comment in step
-      - name: Setup Python & Poetry Environment
+        on:
+          workflow_call:
+            secrets:
+              PYPI_TOKEN:
+                  required: true
+        """
+
+        expected_yaml = """
+        name: Build & Publish
+
+        on:
+          workflow_call:
+            secrets:
+              PYPI_TOKEN:
+                required: true
+        """
+
+        template_to_workflow = TemplateToWorkflow(
+            template_str=cleandoc(input_yaml),
+            github_template_dict=PROJECT_CONFIG.github_template_dict,
+        )
+        assert template_to_workflow.convert() == cleandoc(expected_yaml)
+
+    @staticmethod
+    def test_keeps_comments():
+        input_yaml = """
+        steps:
+          # Comment in nested area
+          - name: SCM Checkout # Comment inline
+            uses: actions/checkout@v6
+            # Comment in step
+        """
+
+        expected_yaml = """
+        steps:
+        # Comment in nested area
+        - name: SCM Checkout # Comment inline
+          uses: actions/checkout@v6
+          # Comment in step
+        """
+
+        template_to_workflow = TemplateToWorkflow(
+            template_str=cleandoc(input_yaml),
+            github_template_dict=PROJECT_CONFIG.github_template_dict,
+        )
+
+        assert template_to_workflow.convert() == cleandoc(expected_yaml)
+
+    @staticmethod
+    def test_keeps_quotes_for_variables_as_is():
+        input_yaml = """
+        - name: Build Artifacts
+          run: poetry build
+        - name: PyPi Release
+          env:
+            POETRY_HTTP_BASIC_PYPI_USERNAME: "__token__"
+            POETRY_HTTP_BASIC_PYPI_PASSWORD: "${{ secrets.PYPI_TOKEN }}"
+          run: poetry publish
+        - name: GitHub Release
+          env:
+            GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          run: >
+            gh release create ${GITHUB_REF_NAME}
+            --title ${GITHUB_REF_NAME}
+            --notes-file ./doc/changes/changes_${GITHUB_REF_NAME}.md
+            dist/*
+        """
+
+        expected_yaml = """
+        - name: Build Artifacts
+          run: poetry build
+        - name: PyPi Release
+          env:
+            POETRY_HTTP_BASIC_PYPI_USERNAME: "__token__"
+            POETRY_HTTP_BASIC_PYPI_PASSWORD: "${{ secrets.PYPI_TOKEN }}"
+          run: poetry publish
+        - name: GitHub Release
+          env:
+            GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          run: >-
+            gh release create ${GITHUB_REF_NAME}
+            --title ${GITHUB_REF_NAME}
+            --notes-file ./doc/changes/changes_${GITHUB_REF_NAME}.md
+            dist/*
+        """
+
+        template_to_workflow = TemplateToWorkflow(
+            template_str=cleandoc(input_yaml),
+            github_template_dict=PROJECT_CONFIG.github_template_dict,
+        )
+
+        assert template_to_workflow.convert() == cleandoc(expected_yaml)
+
+    @staticmethod
+    def test_updates_jinja_variables():
+        input_yaml = """
+        - name: Setup Python & Poetry Environment
+          uses: exasol/python-toolbox/.github/actions/python-environment@v5
+          with:
+            python-version: "(( minimum_python_version ))"
+            poetry-version: "(( dependency_manager_version ))"
+        """
+        expected_yaml = """
+        - name: Setup Python & Poetry Environment
         uses: exasol/python-toolbox/.github/actions/python-environment@v5
         with:
           python-version: "3.10"
           poetry-version: "2.3.0"
-      - name: Build Artifacts
-        run: poetry build
-      - name: PyPi Release
-        env:
-          POETRY_HTTP_BASIC_PYPI_USERNAME: "__token__"
-          POETRY_HTTP_BASIC_PYPI_PASSWORD: "${{ secrets.PYPI_TOKEN }}"
-        run: poetry publish
-      - name: GitHub Release
-        env:
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-        run: >
-          gh release create ${GITHUB_REF_NAME}
-          --title ${GITHUB_REF_NAME}
-          --notes-file ./doc/changes/changes_${GITHUB_REF_NAME}.md
-          dist/*
+        """
 
-"""
-
-
-class TestTemplateToWorkflow:
-    @staticmethod
-    def test_works_as_expected():
         template_to_workflow = TemplateToWorkflow(
-            template_str=TEMPLATE,
+            template_str=cleandoc(input_yaml),
             github_template_dict=PROJECT_CONFIG.github_template_dict,
         )
-        assert template_to_workflow.convert() == cleandoc(WORKFLOW)
+
+        assert template_to_workflow.convert() == cleandoc(expected_yaml)
+
+    @staticmethod
+    def test_preserves_list_format():
+        input_yaml = """
+        on:
+          pull_request:
+            types: [opened, synchronize, reopened]
+
+        Type-Check:
+          name: Type Checking (Python-${{ matrix.python-versions }})
+          runs-on: "ubuntu-24.04"
+          permissions:
+            contents: read
+          strategy:
+            fail-fast: false
+            matrix:
+              python-versions: ["3.10", "3.11", "3.12", "3.13", "3.14"]
+        """
+
+        template_to_workflow = TemplateToWorkflow(
+            template_str=cleandoc(input_yaml),
+            github_template_dict=PROJECT_CONFIG.github_template_dict,
+        )
+
+        assert template_to_workflow.convert() == cleandoc(input_yaml)
