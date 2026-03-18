de7a3cbec3a3fe758a751c68a4a11eb56ea04232
ckunki <christoph.kuhnke@exasol.com>
2026-03-18 17:28:17 +0100

    Added docstring

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -15,4 +15,12 @@ def poetry_path() -> str:
 
 @pytest.fixture(scope="session")
 def ptb_minimum_python_version() -> str:
+    """
+    Some integration tests create a sample poetry project and need to
+    specify its minimum python version in property "requires-python" in file
+    pyproject.toml.
+
+    This fixture returns a value including all python versions supported by
+    the PTB.
+    """
     return BaseConfig(root_path=Path(), project_name="toolbox").minimum_python_version
