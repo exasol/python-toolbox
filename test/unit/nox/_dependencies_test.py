9bd15c9c1663a31ad8f138de18288676174f92d9
ckunki <christoph.kuhnke@exasol.com>
2026-04-06 12:03:39 +0200

    Added integration test

--- test/unit/nox/_dependencies_test.py
+++ test/unit/nox/_dependencies_test.py
@@ -1,18 +1,25 @@
-from unittest import mock
+from unittest.mock import Mock
 
-from exasol.toolbox.nox._dependencies import audit
+from exasol.toolbox.nox import _dependencies
 from exasol.toolbox.util.dependencies.audit import Vulnerabilities
 
 
-class TestAudit:
-    @staticmethod
-    def test_works_as_expected_with_mock(nox_session, sample_vulnerability, capsys):
-        with mock.patch(
-            "exasol.toolbox.nox._dependencies.Vulnerabilities"
-        ) as mock_class:
-            mock_class.load_from_pip_audit.return_value = Vulnerabilities(
-                vulnerabilities=[sample_vulnerability.vulnerability]
-            )
-            audit(nox_session)
+# Proposal: Remove this test and the related nox task under test
+def test_audit(monkeypatch, nox_session, sample_vulnerability, capsys):
+    monkeypatch.setattr(_dependencies, "Vulnerabilities", Mock())
+    _dependencies.Vulnerabilities.load_from_pip_audit.return_value = Vulnerabilities(
+        vulnerabilities=[sample_vulnerability.vulnerability]
+    )
+    _dependencies.audit(nox_session)
+    assert capsys.readouterr().out == sample_vulnerability.nox_dependencies_audit
 
-        assert capsys.readouterr().out == sample_vulnerability.nox_dependencies_audit
+
+def test_report_resolved_vulnerabilities(monkeypatch, nox_session, capsys, sample_vulnerability):
+    monkeypatch.setattr(
+        _dependencies,
+        "get_vulnerabilities_from_latest_tag",
+        Mock(return_value=[sample_vulnerability.vulnerability]),
+    )
+    monkeypatch.setattr(_dependencies, "get_vulnerabilities", Mock(return_value=[]))
+    _dependencies.report_resolved_vulnerabilities(nox_session)
+    assert "| jinja2 | CVE-2025-27516 | 3.1.5 | 3.1.6 |" in capsys.readouterr().out
