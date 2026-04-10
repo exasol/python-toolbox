f71604c751caa24a4117477d2b8826d4f976d8e5
ckunki <christoph.kuhnke@exasol.com>
2026-04-10 12:36:02 +0200

    Refactored tests

--- test/unit/util/release/test_markdown.py
+++ test/unit/util/release/test_markdown.py
@@ -9,6 +9,10 @@ from exasol.toolbox.util.release.markdown import (
 )
 
 
+def _markdown(text: str) -> Markdown:
+    return Markdown.from_text(cleandoc(text))
+
+
 class Scenario:
     def __init__(
         self, initial: str, expected_output: str, expected_children: list[str]
@@ -20,14 +24,6 @@ class Scenario:
     def create_testee(self) -> Markdown:
         return Markdown.from_text(self.initial)
 
-
-INVALID_MARKDOWN = cleandoc("""
-    # Title
-
-    Some text.
-
-    # Another Title
-    """)
 
 MINIMAL = Scenario(
     initial="""
@@ -42,27 +38,31 @@ MINIMAL = Scenario(
     expected_children=[],
 )
 
-WITH_CHILD = Scenario(
+FULL = Scenario(
     initial="""
-    # Parent
-    text
+    # title
+    intro
+    * item one
+    * item two
     ## Child
-    paragraph
-
-    * item 1
-    * item 2
+    cintro
+    - item c1
+    - item c2
     """,
     expected_output="""
-    # Parent
+    # title
 
-    text
+    intro
+
+    * item one
+    * item two
 
     ## Child
 
-    paragraph
+    cintro
 
-    * item 1
-    * item 2
+    - item c1
+    - item c2
     """,
     expected_children=["## Child"],
 )
@@ -124,15 +124,15 @@ NESTED = Scenario(
     expected_children=["## Child A", "## Child B"],
 )
 
+CHILD = _markdown("""
+    ## Sample Child
+    child intro.
+    """)
 
-@pytest.fixture
-def sample_child() -> Markdown:
-    return Markdown(title="## New", intro="intro")
-
-
-@pytest.fixture
-def illegal_child() -> Markdown:
-    return Markdown(title="# Top-level", intro="intro")
+ILLEGAL_CHILD = _markdown("""
+   # Top-level
+   intro
+   """)
 
 
 def test_no_title_error():
@@ -141,33 +141,87 @@ def test_no_title_error():
 
 
 def test_additional_line_error():
+    invalid_markdown = cleandoc("""
+    # Title
+    Some text.
+    # Another Title
+    """)
+    
     expected_error = (
         'additional line "# Another Title" after top-level section "# Title".'
     )
     with pytest.raises(ParseError, match=expected_error):
-        Markdown.from_text(INVALID_MARKDOWN)
+        Markdown.from_text(invalid_markdown)
 
 
-def test_constructor_illegal_child(illegal_child: Markdown):
+def test_constructor_illegal_child():
     with pytest.raises(IllegalChild):
-        Markdown("# title", children=[illegal_child])
+        Markdown("# title", children=[ILLEGAL_CHILD])
 
 
-def test_no_intro() -> None:
-    content = """
-    # title
-    * #123: Fixed vulnerability
-    """
-    testee = Markdown.from_text(cleandoc(content))
-    assert testee == Markdown("# title", "", "* #123: Fixed vulnerability")
+@pytest.mark.parametrize("content, expected", [
+    pytest.param(
+        """
+        # title
+        """,
+        Markdown("# title"),
+        id="only_title",
+    ),
+    pytest.param(
+        """
+        # title
+        intro
+        """,
+        Markdown("# title", "intro"),
+        id="intro",
+    ),
+    pytest.param(
+        """
+        # title
+        * item 1
+        """,
+        Markdown("# title", "", "* item 1"),
+        id="items",
+    ),
+    pytest.param(
+        """
+        # title
+        intro
+        * item 1
+        * item 2
+        """,
+        Markdown("# title", "intro", "* item 1\n* item 2"),
+        id="intro_and_items",
+    ),
+    pytest.param(
+        """
+        # title
+        intro
+        - item 1
+        - item 2
+        """,
+        Markdown("# title", "intro", "- item 1\n- item 2"),
+        id="intro_dash_items",
+    ),
+])
+def test_equals(content: str, expected: Markdown) -> None:
+    assert Markdown.from_text(cleandoc(content)) == expected
 
 
-def test_equals() -> None:
-    testee = MINIMAL.create_testee()
-    other = MINIMAL.create_testee()
-    assert other == testee
-    other.title = "# other"
-    assert other != testee
+@pytest.mark.parametrize(
+    "attr, value",
+    [
+        ("title", "# other"),
+        ("intro", "other"),
+        ("items", "- aaa"),
+        ("children", []),
+    ],
+)
+def test_different(attr, value) -> None:
+    testee = FULL.create_testee()
+    other = FULL.create_testee()
+    setattr(other, attr, value)
+    assert testee != other
 
 
 def test_test_read(tmp_path) -> None:
@@ -176,7 +230,7 @@ def test_test_read(tmp_path) -> None:
     assert Markdown.read(file) == MINIMAL.create_testee()
 
 
-ALL_SCENARIOS = [MINIMAL, WITH_CHILD, TWO_CHILDREN, NESTED]
+ALL_SCENARIOS = [MINIMAL, FULL, TWO_CHILDREN, NESTED]
 
 
 @pytest.mark.parametrize("scenario", ALL_SCENARIOS)
@@ -203,25 +257,25 @@ def test_rendered(scenario: Scenario):
     "scenario, pos",
     [
         (MINIMAL, 0),
-        (WITH_CHILD, 1),
+        (FULL, 1),
         (TWO_CHILDREN, 1),
     ],
 )
-def test_add_child(sample_child: Markdown, scenario: Scenario, pos: int):
+def test_add_child(scenario: Scenario, pos: int):
     testee = scenario.create_testee()
-    testee.add_child(sample_child)
-    assert testee.children[pos] == sample_child
+    testee.add_child(CHILD)
+    assert testee.children[pos] == CHILD
 
 
-def test_replace_illegal_child(illegal_child):
-    testee = WITH_CHILD.create_testee()
+def test_replace_illegal_child():
+    testee = FULL.create_testee()
     with pytest.raises(IllegalChild):
-        testee.replace_or_append_child(illegal_child)
+        testee.replace_or_append_child(ILLEGAL_CHILD)
 
 
 @pytest.mark.parametrize("scenario", ALL_SCENARIOS)
 def test_replace_existing_child(scenario: Scenario):
-    testee = WITH_CHILD.create_testee()
+    testee = FULL.create_testee()
     old_child = testee.children[0]
     old_rendered = testee.rendered
     new_child = Markdown(old_child.title, "new intro")
@@ -231,19 +285,19 @@ def test_replace_existing_child(scenario: Scenario):
 
 
 @pytest.mark.parametrize("scenario", ALL_SCENARIOS)
-def test_replace_non_existing_child(scenario: Scenario, sample_child: Markdown):
+def test_replace_non_existing_child(scenario: Scenario):
     testee = scenario.create_testee()
     expected = len(testee.children) + 1
-    testee.replace_or_append_child(sample_child)
+    testee.replace_or_append_child(CHILD)
     assert len(testee.children) == expected
-    assert testee.children[-1] == sample_child
+    assert testee.children[-1] == CHILD
 
 
 @pytest.mark.parametrize("scenario", ALL_SCENARIOS)
-def test_add_illegal_child(illegal_child: Markdown, scenario: Scenario):
+def test_add_illegal_child(scenario: Scenario):
     testee = scenario.create_testee()
     with pytest.raises(IllegalChild):
-        testee.add_child(illegal_child)
+        testee.add_child(ILLEGAL_CHILD)
 
 
 def test_nested():
