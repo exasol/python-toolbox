b732144ef75fd5371d21008f8737623db78fbcd9
ckunki <christoph.kuhnke@exasol.com>
2026-04-13 14:45:31 +0200

    fixed unit test

--- test/unit/util/dependencies/track_vulnerabilities_test.py
+++ test/unit/util/dependencies/track_vulnerabilities_test.py
@@ -5,30 +5,14 @@ from exasol.toolbox.util.dependencies.track_vulnerabilities import (
 )
 
 
-def _flip_id_and_alias(vulnerability: SampleVulnerability):
-    other = vulnerability
-    vuln_entry = {
-        "aliases": [other.vulnerability_id],
-        "id": other.cve_id,
-        "fix_versions": other.vulnerability.fix_versions,
-        "description": other.description,
-    }
-    return Vulnerability.from_audit_entry(
-        package_name=other.package_name,
-        version=other.version,
-        vuln_entry=vuln_entry,
-    )
-
-
 class TestVulnerabilityMatcher:
     def test_not_resolved(self, sample_vulnerability):
         vuln = sample_vulnerability.vulnerability
         matcher = VulnerabilityMatcher(current_vulnerabilities=[vuln])
         assert not matcher.is_resolved(vuln)
 
-    def test_changed_id_not_resolved(self, sample_vulnerability):
-        vuln2 = _flip_id_and_alias(sample_vulnerability)
-        matcher = VulnerabilityMatcher(current_vulnerabilities=[vuln2])
+    def test_changed_id_not_resolved(self, sample_vulnerability, flipped_id_vulnerability):
+        matcher = VulnerabilityMatcher(current_vulnerabilities=[flipped_id_vulnerability])
         assert not matcher.is_resolved(sample_vulnerability.vulnerability)
 
     def test_resolved(self, sample_vulnerability):
