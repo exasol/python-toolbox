b72d19a51d7a3fa611722f8d3957894a5413129d
Ariel Schulz <ariel.schulz@exasol.com>
2026-05-13 12:11:56 +0200

    Fix typo

--- test/unit/util/workflows/workflow_test.py
+++ test/unit/util/workflows/workflow_test.py
@@ -187,7 +187,7 @@ class TestUpdateWorkflow:
         assert result[:10] == input_text[:10]
 
     @staticmethod
-    def test_not_maintaed_workflows_added_to_new_project(
+    def test_not_maintained_workflows_added_to_new_project(
         project_config_without_patcher,
     ):
         directory = project_config_without_patcher.github_workflow_directory
