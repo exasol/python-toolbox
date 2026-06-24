d9c950dbc49cbbb682d79b2e2bb56957ed33ad74
jana-selva <janaarthanan.selvarajan@exasol.com>
2026-06-24 17:27:14 +0530

    fixed review comments

--- test/integration/tools/security_integration_test.py
+++ test/integration/tools/security_integration_test.py
@@ -88,9 +88,13 @@ class TestFilter:
         json_path = tmp_path / "input.json"
         json_path.write_text(sample_maven_vulnerabilities.issues_json)
 
-        result = cli_runner.invoke(
-            CVE_CLI, ["filter", Filter.GitHubIssues.value, str(json_path)]
-        )
+        with patch(
+            "exasol.toolbox.tools.security.gh_security_issues",
+            return_value=(),
+        ):
+            result = cli_runner.invoke(
+                CVE_CLI, ["filter", Filter.GitHubIssues.value, str(json_path)]
+            )
 
         assert result.exit_code == 0
         assert result.output.strip() == sample_maven_vulnerabilities.issues_json
